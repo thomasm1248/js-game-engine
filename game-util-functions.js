9c0b6c65d745733adce77fe01b288ad5c53baf5e
thomasm1248 <thomasm1248@gmail.com>
2023-02-02 11:19:49 -0700

    Add collision code

--- game-util-functions.js
+++ game-util-functions.js
@@ -108,3 +108,81 @@ function bindObjectToCanvas(object, canvas) {
 	if(object.pos.y < object.box.y) object.pos.y = object.box.y;
 	if(object.pos.y > canvas.height - object.box.y) object.pos.y = canvas.height - object.box.y;
 }
+
+
+
+// Physics
+
+/*
+This method assumes that each input object is round, and has pos,
+vel, and mass attributes.
+*/
+function calculateElasticCollision(ball1, ball2) {
+    // Get normal that collision will happen on
+    normal = ball2.pos.subtract(ball1.pos).normalize();
+
+    // Project velocities of both balls onto normal
+    var v1 = ball1.vel.dot(normal);
+    var v2 = ball2.vel.dot(normal);
+
+    // Use formula to calculate final velocities
+    var v1f = (v1 * (ball1.mass - ball2.mass) + 2 * ball2.mass * v2) / (ball1.mass + ball2.mass);
+    var v2f = (v2 * (ball2.mass - ball1.mass) + 2 * ball1.mass * v1) / (ball2.mass + ball1.mass);
+
+    // Apply changes to the velocities of each ball
+    ball1.vel.accum(normal.scale(v1f - v1));
+    ball2.vel.accum(normal.scale(v2f - v2));
+
+    // Figure out how much they overlap
+    var overlap = ball1.rad + ball2.rad - ball1.pos.dist(ball2.pos);
+
+    // Calculate how much to shift each ball based on the overlap and their masses
+    var totalMass = ball1.mass + ball2.mass;
+    var ball1Shift = ball2.mass / totalMass * overlap;
+    var ball2Shift = ball1.mass / totalMass * overlap;
+
+    // Apply the calculated shift
+    ball1.pos.accum(normal.scale(-ball1Shift));
+    ball2.pos.accum(normal.scale(ball2Shift));
+}
+
+function elasticCollisions(balls) {
+    for(var i = 0; i < balls.length; i++) {
+        var b1 = balls[i];
+        for(var j = i + 1; j < balls.length; j++) {
+            var b2 = balls[j];
+
+            if(b1.pos.dist(b2.pos) <= b1.rad + b2.rad) {
+                calculateElasticCollision(b1, b2);
+            }
+        }
+    }
+}
+
+function applyFriction(objects, friAmount) {
+	for(var i = 0; i < objects.length; i++) {
+        var norm = objects[i].vel.norm();
+        objects[i].vel = objects[i].vel.scale(norm <= friAmount ? 0 : (norm - friAmount) / norm);
+	}
+}
+
+// Each object needs a box
+function bounceWithinCanvas(objects, canvas) {
+	if(objects[i].pos.x < objects[i].box.x) objects[i].vel.x *= -1;
+	if(objects[i].pos.y < objects[i].box.y) objects[i].vel.y *= -1;
+	if(objects[i].pos.x > canvas.width - objects[i].box.x) objects[i].vel.x *= -1;
+	if(objects[i].pos.y > canvas.height - objects[i].box.y) objects[i].vel.y *= -1;
+	bindObjectToCanvas(objects[i], canvas);
+}
+
+
+
+// Game Management
+
+function simpleObjectUpdateManager(objects, model) {
+	for(var i = 0; i < objects.length; i++) {
+		if(objects[i].update(model)) {
+			objects.splice(i--, 1);
+		}
+	}
+}
